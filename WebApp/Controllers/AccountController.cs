e7a999627aa64bd89dbbfbd5ccce110e7f149d9e
pytas0811 <105399564+pytas0811@users.noreply.github.com>
2023-10-13 03:56:57 +0700

    Add project files.

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿
+using DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    [Controller]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = registerViewModel.Email,
+                    Email = registerViewModel.Email,
+                    Name = registerViewModel.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index");
+                }
+                AddError(result);
+            }
+            return View(registerViewModel);
+        }
+
+        private void AddError(IdentityResult result)
+        {
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
---
+++ WebApp/Pages/Shared/_LoginPartialCustom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Shared
+{
+    public class _LoginPartialCustomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
